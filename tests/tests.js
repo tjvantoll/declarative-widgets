fa0334fc7fbed66e7fe96af22e5b8b79dcec9129
TJ VanToll <tj.vantoll@gmail.com>
2014-01-28 23:07:02 -0500

    Fill out test suite. Fixes gh-2.

--- tests/tests.js
+++ tests/tests.js
@@ -3,7 +3,51 @@ require.config({
 });
 
 require([ "declarative-widgets" ], function( declarative ) {
-	test( "Verify public API", function() {
+	var fixture = $( "#qunit-fixture" );
+	$.widget( "tj.test", {
+		options: {
+			one: "string",
+			two: false,
+			three: true,
+			four: 0
+		}
+	});
+
+	test( "Public API", function() {
+		expect( 1 );
 		equal( typeof declarative.initWidgets, "function" );
+	});
+
+	test( "Attribute reading", function() {
+		expect( 4 );
+		var options,
+			div = $( "<div>", {
+				"data-one": "foo",
+				"data-two": true,
+				"data-three": false,
+				"data-four": 4
+			});
+
+		fixture.append( div );
+		div.test();
+
+		options = div.test( "instance" ).options;
+		equal( options.one, "foo" );
+		equal( options.two, true );
+		equal( options.three, false );
+		equal( options.four, 4 );
+	});
+
+	test( "initWidgets plugin", function() {
+		expect( 2 );
+		var div = $( "<div>", {
+			"data-widget": "test",
+			"data-one": "bar"
+		});
+
+		fixture.append( div ).initWidgets();
+
+		ok( div.is( ":tj-test" ) );
+		equal( div.test( "instance" ).options.one, "bar" );
 	});
 });
